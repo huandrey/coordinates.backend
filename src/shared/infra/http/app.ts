e0843ac023f430ef45f3198faf7a506298f16cef
Huandrey Pontes <huandrey.pontes@ccc.ufcg.edu.br>
2022-09-08 19:51:35 -0300

    add func

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -15,8 +15,17 @@ AppDataSource.initialize().then(() => {
 
   app.use(express.json());
   app.use(cors());
-  app.use(router);
 
+  app.use('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    next();
+  });
+
+  app.use(router);
+  
   app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
